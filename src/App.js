330cf335a8db0a90f8aadc57b35a5b7e23cc479f
spettigrew <sara.pettigrew5@gmail.com>
2020-02-17 21:37:28 -0700

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import createTodoForm from './Todos/createTodoForm'
+import useTodos from '../hooks/useTodos'
+import './App.css'
 
-const App = () => (
-  <div className="App">
-    <p>React starter - Chrome Extension</p>
-  </div>
-);
+const App = () => {
+  const [todos, addTodo, setTodoCompleted] = useTodos()
+
+  return (
+    <div className="App">
+      <div className="todo-container">
+        <h2>Your Todos List</h2>
+        <hr />
+        {todos.map(todo => (
+          <div className="todo">
+            <p>{todo.label}</p>
+            <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => setTodoCompleted(todo.id, !todo.completed)}
+            />
+          </div>
+        ))}
+    </div>
+    <createTodoForm createNewTodo={addTodo} />
+    </div>
+  )
+}
 
 export default App;
